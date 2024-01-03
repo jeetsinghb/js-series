cdfe4cfec0d1c718a57b568c5b092b583c6c71c3
Tarjeet Singh <33022548+jeetsinghb@users.noreply.github.com>
2024-01-03 04:32:02 +0000

    Understanding Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+// JS engine is in browser
+
+
+let name = "Tarjeet"; // string value
+let age = 26; // number value
+let isLoggedIn = true; // boolean value
+
+
+// Primitive Data Types
+
+// number
+// bigint
+// string => ""
+// boolean => true/galse
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// Non-Primitive Data Types
+// Objects
+
+console.log(typeof undefined); // undefined => value
+console.log(typeof null); // object
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # js-series
 A code repo for javascript series at Chai aur Code YT Channel
+
+
+# References
+# https://tc39.es/ecma262/
+# https://developer.mozilla.org/en-US/
+
