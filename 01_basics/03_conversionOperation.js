e8bcb7de0de0c43374fde3776f128e2b9dd52325
Tarjeet Singh <33022548+jeetsinghb@users.noreply.github.com>
2024-01-08 00:41:26 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+const score = undefined; // null, undefined, boolean, string
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => Nan
+
+let isLoggedIn = "Tarjeet"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 = false
+// "" => false
+// "hitest" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
