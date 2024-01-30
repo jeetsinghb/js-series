086d1c85fb355d186c3bd9420525ee9ed9a54748
Tarjeet Singh <33022548+jeetsinghb@users.noreply.github.com>
2024-01-30 06:28:29 +0530

    Update 06_nums_and_maths.js

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -6,12 +6,17 @@ console.log(score);
 const balance = new Number(100)
 console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toString().length);
-console.log(balance.toFixed(2)); // Decimal
+console.log(balance.toString()); // converts to string
+console.log(balance.toString().length); // 3
+console.log(balance.toFixed(2)); // 100.00 - Decimal value
 
 const otherNumber = 1123.8966;
 console.log(otherNumber.toPrecision(3)); // precise value
+/*
+Eg:
+const otherNumber = 33.8966;
+console.log(otherNumber.toPrecision(3)); // 33.9
+*/
 
 const hundreds = 1000000;
 console.log(hundreds.toLocaleString()); // US based
