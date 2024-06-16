405bb2b4be13ed56b3940e10fc43618fff2066fe
Tarjeet Singh <33022548+jeetsinghb@users.noreply.github.com>
2024-06-16 14:46:33 +0530

    Update 06_nums_and_maths.js

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -59,7 +59,7 @@ console.log(Math.floor(4.9)); // 4
 console.log(Math.min(4, 3, 6, 8)); // 3
 console.log(Math.max(4, 3, 6, 8)); // 8
 
-console.log(Math.random()); // value comes between 1 to 0
+console.log(Math.random()); // value comes between 0 to 1
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
 
