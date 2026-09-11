3a309fc0061377bdf77956f2c00865cbaf62822e
Tarjeet Singh <33022548+jeetsinghb@users.noreply.github.com>
2026-09-11 18:32:24 +0000

    while and do while

---
+++ 05_iterations/02_while_do_while.js
@@ -0,0 +1,37 @@
+// while
+
+let index = 0;
+
+while (index <= 10) {
+    // console.log(`value of index is: ${index}`);
+    // index++;
+    index = index + 2;
+}
+
+
+let num = 0;
+while (num <= 10) {
+    // console.log(`num is: ${num}`);
+    num = num + 2;
+}
+
+
+let myArray = ['flash', 'batman', 'superman'];
+
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    // arr = arr + 1;
+    arr++;
+}
+
+
+// do while
+
+let score = 1; // try 11
+
+// task
+do {
+    console.log(`Score is: ${score}`); // it will run first and checks condition later
+    score++;
+} while (score <= 10) // condition check
